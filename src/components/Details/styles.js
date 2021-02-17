1ae7c69b12e7865561d7b9e2caeeba70661c2062
Yousef Hany <yousefhany@protonmail.com>
2021-02-17 02:55:19 +0200

    Added Components and UI with dumm text

---
+++ src/components/Details/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles, useStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+  income: {
+    borderBottom: "10px solid rgba(30,255,150,0.7)",
+  },
+  expense: {
+    borderBottom: "10px solid rgba(30,150,255,0.7)",
+  },
+}));
